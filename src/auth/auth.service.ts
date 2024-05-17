af303eef6366329089ec6a85d529afd40465ab46
Damian Polewski <damian.polewski.1tib@gmail.com>
2024-05-17 20:26:03 +0200

    feat: add signup logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,20 +5,32 @@ import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private config: ConfigService,
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
 
   async signupLocal(dto: AuthDto): Promise<Tokens> {
     const hash = await argon.hash(dto.password);
 
-    const user = this.prisma.user.create({
+    const newUser = await this.prisma.user.create({
       data: {
         email: dto.email,
         hash,
       },
     });
+
+    const tokens = await this.signToken(newUser.id, newUser.email);
+
+    await this.updateRefreshTokenHash(newUser.id, tokens.refresh_token);
+
+    return tokens;
   }
 
   signinLocal() {}
@@ -26,4 +38,39 @@ export class AuthService {
   logout() {}
 
   refreshTokens() {}
+
+  async updateRefreshTokenHash(userId: number, refreshToken: string) {
+    const hash = await argon.hash(refreshToken);
+
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRt: hash,
+      },
+    });
+  }
+
+  async signToken(userId: number, email: string): Promise<Tokens> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+
+    const accessToken = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('ACCESS_TOKEN_SECRET'),
+    });
+
+    const refreshToken = await this.jwt.signAsync(payload, {
+      expiresIn: '7d',
+      secret: this.config.get('REFRESH_TOKEN_SECRET'),
+    });
+
+    return {
+      access_token: accessToken,
+      refresh_token: refreshToken,
+    };
+  }
 }
