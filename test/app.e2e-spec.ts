a2f723fee9d868c2df5fe5c4aed7144e08c6b466
Damian Polewski <damian.polewski.1tib@gmail.com>
2024-05-20 22:36:08 +0200

    chore: add auth module e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,11 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+
+import * as pactum from 'pactum';
+
 import { AppModule } from '../src/app.module';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -19,14 +23,154 @@ describe('App e2e', () => {
       }),
     );
     await app.init();
+    await app.listen(8080);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+
+    pactum.request.setBaseUrl('http://localhost:8080');
   });
 
   afterAll(() => {
     app.close();
   });
 
-  it.todo('should pass');
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'test@example.com',
+      password: '123',
+    };
+
+    describe('Signup', () => {
+      it('should throw if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/local/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/local/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if no body provided', () => {
+        return pactum.spec().post('/auth/local/signup').expectStatus(400);
+      });
+
+      it('should singup', () => {
+        return pactum
+          .spec()
+          .post('/auth/local/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      });
+    });
+
+    describe('Signin', () => {
+      it('should throw if no body provided', () => {
+        return pactum.spec().post('/auth/local/signin').expectStatus(400);
+      });
+
+      it('should throw if email is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/local/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      });
+
+      it('should throw if password is empty', () => {
+        return pactum
+          .spec()
+          .post('/auth/local/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      });
+
+      it('should signin', () => {
+        return pactum
+          .spec()
+          .post('/auth/local/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAt', 'accessToken')
+          .stores('userRt', 'refreshToken');
+      });
+    });
+
+    describe('Refresh', () => {
+      it('should throw if refreshToken not provided', () => {
+        return pactum.spec().post('/auth/refresh').expectStatus(401);
+      });
+
+      it('should throw if wrong accessToken provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/refresh')
+          .withHeaders({
+            Authorization: 'Bearer 123',
+          })
+          .expectStatus(401);
+      });
+
+      it('should refresh', () => {
+        return pactum
+          .spec()
+          .post('/auth/refresh')
+          .withHeaders({
+            Authorization: 'Bearer $S{userRt}',
+          })
+          .expectStatus(200);
+      });
+    });
+
+    describe('Logout', () => {
+      it('should throw if no accessToken provided', () => {
+        return pactum.spec().post('/auth/logout').expectStatus(401);
+      });
+
+      it('should throw if wrong accessToken provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/logout')
+          .withHeaders({
+            Authorization: 'Bearer abc',
+          })
+          .expectStatus(401);
+      });
+
+      it('should logout', () => {
+        return pactum
+          .spec()
+          .post('/auth/logout')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}',
+          })
+          .expectStatus(200);
+      });
+
+      it('should not refresh after logout', () => {
+        return pactum
+          .spec()
+          .post('/auth/refresh')
+          .withHeaders({
+            Authorization: 'Bearer $S{userRt}',
+          })
+          .expectStatus(403);
+      });
+    });
+  });
 });
